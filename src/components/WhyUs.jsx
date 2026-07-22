1bc0bbbc3e13beb6fb72d17dd7b92365569ba6da
sunil979191 <karthicsk10@gmail.com>
2026-07-22 18:49:31 +0530

    f

--- src/components/WhyUs.jsx
+++ src/components/WhyUs.jsx
@@ -1,6 +1,7 @@
 import "./WhyUs.css";
 import { advantages, awards } from "../data/content";
 import Icon from "./Icons";
+import leaderAward from "../assets/clients/leader.jpeg";
 
 export default function WhyUs() {
   return (
@@ -21,21 +22,29 @@ export default function WhyUs() {
         </div>
 
         <div className="awards">
-          <div className="section-head section-head--compact">
-            <span className="section-eyebrow">Compliance &amp; Recognition</span>
-            <h3>Certified, compliant, and recognized by industry leaders</h3>
-          </div>
-          <div className="awards__grid">
-            {awards.map((a) => (
-              <div className="award-card" key={a.title}>
-                <Icon name="fileCheck" size={24} />
-                <h4>{a.title}</h4>
-                <p>{a.description}</p>
+          <div className="awards__inner">
+            <div className="awards__photo">
+              <img src={leaderAward} alt="Mspring Infotech receiving the Most Promising Company of the Year 2023 award at the Business Mint Nation Wide Awards" />
+            </div>
+            <div className="awards__content">
+              <div className="section-head section-head--compact">
+                <span className="section-eyebrow">Compliance &amp; Recognition</span>
+                <h3>Certified, compliant, and recognized by industry leaders</h3>
               </div>
-            ))}
+              <div className="awards__grid">
+                {awards.map((a) => (
+                  <div className="award-card" key={a.title}>
+                    <Icon name="fileCheck" size={24} />
+                    <h4>{a.title}</h4>
+                    <p>{a.description}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </section>
+    
   );
 }
